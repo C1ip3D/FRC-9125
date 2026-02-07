5449859aff890138e637d729195759073f8c7496
Avaneesh Shetti <shettiavaneesh@gmail.com>
2026-02-07 15:31:01 -0800

    New Swerve

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,104 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
 
-public final class Constants {
-    public static final class DriveConstants {
-        // Driving Parameters - Note: these are example values, must be tuned for actual
-        // robot
-        public static final double MAX_SPEED_METERS_PER_SECOND = Units.feetToMeters(10.0); // ~3.048 m/s
-        public static final double MAX_ANGULAR_SPEED = 6.0; // radians per second
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants
+{
 
-        // Chassis configuration
-        public static final double TRACK_WIDTH = Units.inchesToMeters(24.0); // Distance between centers of right and
-                                                                             // left wheels on robot
-        public static final double WHEEL_BASE = Units.inchesToMeters(24.0); // Distance between centers of front and
-                                                                            // back wheels on robot
 
-        public static final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(
-                new Translation2d(WHEEL_BASE / 2, TRACK_WIDTH / 2),
-                new Translation2d(WHEEL_BASE / 2, -TRACK_WIDTH / 2),
-                new Translation2d(-WHEEL_BASE / 2, TRACK_WIDTH / 2),
-                new Translation2d(-WHEEL_BASE / 2, -TRACK_WIDTH / 2));
+  // APRIL TAG CONSTANTS
 
-        // Angular offsets of the modules relative to the chassis in radians
-        public static final double FRONT_LEFT_CHASSIS_ANGULAR_OFFSET = 0.041016 * 2 * Math.PI;
-        public static final double FRONT_RIGHT_CHASSIS_ANGULAR_OFFSET = 0.882812 * 2 * Math.PI;
-        public static final double BACK_LEFT_CHASSIS_ANGULAR_OFFSET = 0.439454 * 2 * Math.PI;
-        public static final double BACK_RIGHT_CHASSIS_ANGULAR_OFFSET = 0.197754 * 2 * Math.PI;
+  // Auto constants
+	public static final double X_REEF_ALIGNMENT_P = 3.0;
+	public static final double Y_REEF_ALIGNMENT_P = 3.0;
+	public static final double ROT_REEF_ALIGNMENT_P = 0;
 
-        public static final int PIGEON_ID = 13;
-    }
+	public static final double ROT_SETPOINT_REEF_ALIGNMENT = 0;  // Rotation
+	public static final double ROT_TOLERANCE_REEF_ALIGNMENT = 1;
+	public static final double X_SETPOINT_REEF_ALIGNMENT = -0.05;  // Vertical pose
+	public static final double X_TOLERANCE_REEF_ALIGNMENT = 0.02;
+	public static final double Y_SETPOINT_REEF_ALIGNMENT = 0.16;  // Horizontal pose
+	public static final double Y_TOLERANCE_REEF_ALIGNMENT = 0.02;
 
-    public static final class ModuleConstants {
-        // SDS MK4i L2 Gear Ratios
-        public static final double DRIVING_MOTOR_REDUCTION = 6.75;
-        public static final double TURNING_MOTOR_REDUCTION = (150.0 / 7.0); // 21.428571428571427
+	public static final double DONT_SEE_TAG_WAIT_TIME = 1;
+	public static final double POSE_VALIDATION_TIME = 0.3;
 
-        // Calculations required for driving motor conversion factors and feed forward
-        public static final double DRIVING_MOTOR_FREE_SPEED_RPS = 5676.0 / 60;
-        public static final double WHEEL_DIAMETER_METERS = 0.1016; // 4 inches
-        public static final double WHEEL_CIRCUMFERENCE_METERS = WHEEL_DIAMETER_METERS * Math.PI;
 
-        public static final double DRIVE_WHEEL_FREE_SPEED_RPS = (DRIVING_MOTOR_FREE_SPEED_RPS
-                * WHEEL_CIRCUMFERENCE_METERS)
-                / DRIVING_MOTOR_REDUCTION;
 
-        public static final double DRIVING_ENCODER_POSITION_FACTOR = (WHEEL_DIAMETER_METERS * Math.PI)
-                / DRIVING_MOTOR_REDUCTION; // meters per motor rotation
-        public static final double DRIVING_ENCODER_VELOCITY_FACTOR = ((WHEEL_DIAMETER_METERS * Math.PI)
-                / DRIVING_MOTOR_REDUCTION) / 60.0; // meters per second per motor RPM
 
-        public static final double TURNING_ENCODER_POSITION_FACTOR = (2 * Math.PI) / TURNING_MOTOR_REDUCTION; // radians
-                                                                                                              // per
-                                                                                                              // motor
-                                                                                                              // rotation
-        public static final double TURNING_ENCODER_VELOCITY_FACTOR = (2 * Math.PI) / TURNING_MOTOR_REDUCTION / 60.0; // radians
-                                                                                                                     // per
-                                                                                                                     // second
-                                                                                                                     // per
-                                                                                                                     // motor
-                                                                                                                     // RPM
+  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+  //changing for now to 3.5 from 14.5: Vihaan
+  public static final double MAX_SPEED  = Units.feetToMeters(10);
+  // Maximum speed of the robot in meters per second, used to limit acceleration.
 
-        public static final double TURNING_ENCODER_POSITION_PID_MIN_INPUT = 0; // radians
-        public static final double TURNING_ENCODER_POSITION_PID_MAX_INPUT = (2 * Math.PI); // radians
+//  public static final class AutonConstants
+//  {
+//
+//    public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
+//    public static final PIDConstants ANGLE_PID       = new PIDConstants(0.4, 0, 0.01);
+//  }
 
-        public static final double DRIVING_P = 0.04;
-        public static final double DRIVING_I = 0;
-        public static final double DRIVING_D = 0;
-        public static final double DRIVING_FF = 1 / DRIVE_WHEEL_FREE_SPEED_RPS;
-        public static final double DRIVING_MIN_OUTPUT = -1;
-        public static final double DRIVING_MAX_OUTPUT = 1;
+  public static final class DrivebaseConstants
+  {
 
-        public static final double TURNING_P = 1;
-        public static final double TURNING_I = 0;
-        public static final double TURNING_D = 0;
-        public static final double TURNING_FF = 0;
-        public static final double TURNING_MIN_OUTPUT = -1;
-        public static final double TURNING_MAX_OUTPUT = 1;
-    }
+    // Hold time on motor brakes when disabled
+    public static final double WHEEL_LOCK_TIME = 10; // seconds
+  }
 
-    public static final class OIConstants {
-        public static final int DRIVER_CONTROLLER_PORT = 0;
-        public static final double DRIVE_DEADBAND = 0.2;
-    }
+  public static class OperatorConstants
+  {
 
-    public static final class DeviceIDs {
-        public static final int FRONT_LEFT_DRIVING_CAN_ID = 2;
-        public static final int FRONT_RIGHT_DRIVING_CAN_ID = 4;
-        public static final int BACK_LEFT_DRIVING_CAN_ID = 6;
-        public static final int BACK_RIGHT_DRIVING_CAN_ID = 8;
+    // Joystick Deadband
+    public static final double DEADBAND        = 0.2;
+    public static final double LEFT_Y_DEADBAND = 0.2;
+    public static final double RIGHT_X_DEADBAND = 0.2;
+    public static final double TURN_CONSTANT    = 6;
 
-        public static final int FRONT_LEFT_TURNING_CAN_ID = 1;
-        public static final int FRONT_RIGHT_TURNING_CAN_ID = 3;
-        public static final int BACK_LEFT_TURNING_CAN_ID = 5;
-        public static final int BACK_RIGHT_TURNING_CAN_ID = 7;
 
-        public static final int FRONT_LEFT_CANCODER_ID = 9;
-        public static final int FRONT_RIGHT_CANCODER_ID = 10;
-        public static final int BACK_LEFT_CANCODER_ID = 11;
-        public static final int BACK_RIGHT_CANCODER_ID = 12;
-    }
+  }
 }
